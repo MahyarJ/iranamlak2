a9facbd97a479269d2186a18f3aa5a07ffba1ce0
MahyarJ <mahyar.jamalabadi@gmail.com>
2014-11-30 01:22:00 +0330

    complete news admin panel and backup db

--- pages/news.php
+++ pages/news.php
@@ -2,6 +2,22 @@
 
 	<?php
 
+		  // $arabic_indic_digits = array(
+		  //   "\xD9\xA0",
+		  //   "\xD9\xA1",
+		  //   "\xD9\xA2",
+		  //   "\xD9\xA3",
+		  //   "\xD9\xA4",
+		  //   "\xD9\xA5",
+		  //   "\xD9\xA6",
+		  //   "\xD9\xA7",
+		  //   "\xD9\xA8",
+		  //   "\xD9\xA9",
+		  // );
+		  // $number = 1234.56;
+		  // $formatted_number = format_number($number, 2);
+		  // $arabic_number = str_replace(array_keys($arabic_indic_digits), $arabic_indic_digits, $formatted_number);
+
 		include_once 'core/database.php';
 
 		$query = "SELECT * FROM `news` ORDER BY `date` DESC";
