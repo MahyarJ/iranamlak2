2f76badb6eea4f46974ddb05a635cc30ce9edbef
pouriamaleki <dev.maleki@gmail.com>
2014-12-04 00:25:14 +0330

    pagination last fix

--- pages/search-result.php
+++ pages/search-result.php
@@ -200,15 +200,12 @@
 	if ($totalCountInt > $count)
 	{
 
-
 		echo "<div class='pagination'>";
 
-		if (0 > $page)
+		if ($totalCountInt > $start + $count - 1)
 		{
-
-			echo createPage(-1, 1, "اولی", $dealType, $estateType, $count, $sortBy, $order, 0);
-			echo createPage(-1, $page-1, "قبلی", $dealType, $estateType, $count, $sortBy, $order, 0);
-
+			echo createPage(-1, $page + 1, "بعدی", $dealType, $estateType, $count, $sortBy, $order, 0);
+			echo createPage(-1, ceil($totalCountInt / $count), "آخری", $dealType, $estateType, $count, $sortBy, $order, 0);
 		}
 
 		for ($i = 0, $j = 1; $i < $totalCountInt; $j++, $i = $i + $count)
@@ -220,10 +217,12 @@
 
 		}
 
-		if ($totalCountInt < $start + $count - 1)
+		if (1 < $page)
 		{
-			echo createPage(-1, $page + 1, "بعدی", $dealType, $estateType, $count, $sortBy, $order, 0);
-			echo createPage(-1, ceil($totalCountInt / $count), "آخری", $dealType, $estateType, $count, $sortBy, $order, 0);
+
+			echo createPage(-1, 1, "اولی", $dealType, $estateType, $count, $sortBy, $order, 0);
+			echo createPage(-1, $page-1, "قبلی", $dealType, $estateType, $count, $sortBy, $order, 0);
+
 		}
 		echo "</div>";
 
